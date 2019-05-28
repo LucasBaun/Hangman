158d216bec6009352214632497c802b6e5a1f345
Lucas Baunsgaard <anonymistan@hotmail.se>
2019-05-28 10:29:31 +0200

    added comments

--- Hang Man/main.py
+++ Hang Man/main.py
@@ -1,11 +1,11 @@
-import random
-import os, time
+import random # Importerar att den ska kunna göra random 
 
-words = []
-file = open('secretwords.txt' , 'r')
-words = file.read().splitlines()
-secret_word = random.choice(words)
+words = [] # Nämner ordet "words" så jag kan använda det senare.
+file = open('secretwords.txt' , 'r') # öppnar och läses filen secretwords.txt
+words = file.read().splitlines() # Säger att den bara ska läsa varje rad som ett objekt.
+secret_word = random.choice(words) # Väljer ut ett random ord.
 
+# Printar ut spelet!
 print("---------------------------------------------------------")
 print("𝐒 𝐊 𝐑 𝐈 𝐕  𝐁 𝐀 𝐑 𝐀  𝐌 𝐄 𝐃  𝐒 𝐌 Å  𝐁 𝐎 𝐊 𝐒 𝐓 Ä 𝐕 𝐄 𝐑")
 print("")
@@ -18,50 +18,47 @@ print("---------------------------------------------------------")
 print("")
 print("")
 
-def get_guess():
+def get_guess(): # Startar en funktion.
 
-  # Lägger _ på alla bokstäver i gömda ordet
-  # ger spelaren 01 liv
-  dashes = "-" * len(secret_word)
-  left = len(secret_word)
-  guesses_left = 10
-  used_letters = []
+  dashes = "-" * len(secret_word) # Lägger _ på alla bokstäver i gömda ordet
+  left = len(secret_word) #  sätter "left" till hur många bokstäver det hemliga ordet har.
+  guesses_left = 10 # Ger spelaren 10 liv
+  used_letters = [] # sätter namnet så den kan användas senare som string
 
 
   # En loop som kollar om du har slut på gissningar eller ifall _ är lika som ordet
   while guesses_left > 0 and not dashes == secret_word:
 
-    # Print the amount of dashes and guesses left
-    print(dashes)
-    print("Bokstäver gissade: ", (used_letters))
-    print ("Liv kvar: ", str(guesses_left))
+    print(dashes) # Skriver ut dashes där bokstäverna är gömda.
+    print("Bokstäver gissade: ", (used_letters)) # Skriver ut vilka bokstäver man gissat på.
+    print ("Liv kvar: ", str(guesses_left)) # Skriver ut hut många gissningar man har kvar
 
     # frågar spelaren efter en gissning
-    guess = input("Gissa:")
-    used_letters.append(guess)
+    guess = input("Gissa:") # Skriver ut en fråga
+    used_letters.append(guess) # Svaret man skriver byts ut ifall det stämmer med secret_word.
 
-    # Varnar ifall man inte skrivern en bokstav eller om man skriver fler än en bokstav
-    if len(guess) != 1:
-      print("")
+    if len(guess) != 1: # Ifall man inte skriver 1 bokstav så kör den det under.
+      # Printar ut att man måste skriva en bokstav.
+      print("") 
       print ("Din bokstav måste vara en bokstav!")
 
     # Ifall man gissar rätt så printar den det och sedan byter ut _ till bokstaven
     elif guess in secret_word:
       print("")
       print ("Du gissade en bokstav rätt!")
-      dashes = update_dashes(secret_word, dashes, guess)
+      dashes = update_dashes(secret_word, dashes, guess) # Uppdaterar dashes så man kan se vart bokstaven var i ordet.
 
 
     # Om spelaren gissar fel så säger programmet det och sedan tar bort ett liv från spelarn
     else:
       print("")
       print ("Du gissade fel.")
-      guesses_left -= 1
+      guesses_left -= 1 # Tar -1 liv från guess_left.
 
-  if guesses_left < 1:
-    print ("Du förlora, ordet var: " + str(secret_word))
+    if guesses_left < 1: # När man har 0 guesses_left så printar den ut vad ordet var.
+      print ("Du förlora, ordet var: " + str(secret_word))
 
-  # Om _ är lika som ordet så har spelaren vunnit.
+  # Om _ är lika som ordet så har spelaren vunnit. Den printar också ut vad ordet var.
   else:
     print ("Grattis! du fick rätt ord: " + str(secret_word))
 
